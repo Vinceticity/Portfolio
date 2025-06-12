9c8a68f3a742eb18aa2520b287fd9d99f465d017
Vinceticity <vincent.macatubal@cvsu.edu.ph>
2025-06-12 05:20:36 +0000

    pront pages

---
+++ script.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper('.slider-wrapper', {
+
+  loop: true,
+  
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    }
+  }
+
+});
